0929132af0b07ca04b78056c8e2e5e6302d714c6
DANCAN AKALL <122867975+odiwuo5805@users.noreply.github.com>
2023-05-26 21:58:50 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -224,8 +224,6 @@ char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
 ssize_t get_node_index(list_t *, list_t *);
-
-/* toem_vars.c */
 int is_chain(info_t *, char *, size_t *);
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
