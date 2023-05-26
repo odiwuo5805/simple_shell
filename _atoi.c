1e2e4dd3c2084d0bdb7d3a63cf39a890eb2da151
DANCAN AKALL <122867975+odiwuo5805@users.noreply.github.com>
2023-05-26 21:57:53 +0300

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -13,27 +13,27 @@ int interactive(info_t *info)
 
 /**
  * is_delim - checks if character is a delimeter
- * @c: the char to check
+ * @b: the char to check
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+int is_delim(char b, char *delim)
 {
 	while (*delim)
-		if (*delim++ == c)
+		if (*delim++ == b)
 			return (1);
 	return (0);
 }
 
 /**
  *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
+ *@b: The character to input
+ *Return: 1 if b is alphabetic, 0 otherwise
  */
 
-int _isalpha(int c)
+int _isalpha(int b)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z'))
 		return (1);
 	else
 		return (0);
@@ -41,25 +41,25 @@ int _isalpha(int c)
 
 /**
  *_atoi - converts a string to an integer
- *@s: the string to be converted
+ *@w: the string to be converted
  *Return: 0 if no numbers in string, converted number otherwise
  */
 
-int _atoi(char *s)
+int _atoi(char *w)
 {
-	int j, sign = 1, flag = 0, output;
+	int y, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (j = 0;  s[j] != '\0' && flag != 2; j++)
+	for (y = 0;  w[y] != '\0' && flag != 2; y++)
 	{
-		if (s[j] == '-')
+		if (w[y] == '-')
 			sign *= -1;
 
-		if (s[j] >= '0' && s[j] <= '9')
+		if (w[y] >= '0' && w[y] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (s[j] - '0');
+			result += (w[y] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
